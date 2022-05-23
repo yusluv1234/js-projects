ec48b1b63d0e9d642872315a473cec8d1eb3f665
yusluv1234 <yusluv124@gmail.com>
2022-05-24 00:14:38 +0800

    adding best practise revive on Switch and case statement

--- js-understanding-practice/index.js
+++ js-understanding-practice/index.js
@@ -3,9 +3,29 @@
 // If it is between 12pm and 6pm: Good afternoon!
 // Otherwise: Good evening!
 
-let hour = 19;
-if (hour >= 6 && hour <= 12) {
-   console.log("Good Morning!")
-} else if (hour >= 12 && hour <= 18) {
-  console.log("Good Afternoon!")
-} else console.log("Good Evening!")
+// let hour = 19;
+// if (hour >= 6 && hour <= 12) 
+//    console.log("Good Morning!")
+//  else if (hour >= 12 && hour <= 18) 
+//   console.log("Good Afternoon!")
+//  else
+//  console.log("Good Evening!")
+
+let role;
+switch(role) {
+  case 'guest':
+  console.log('Guest User')
+  break;
+  case 'moderator':
+    console.log('Moderator User')
+    break;
+    default:
+      console.log('Unknown User')
+}
+
+// Using If and Else for this switch and case
+if (role === 'moderator')
+  console.log('Moderator User');
+else if (role === 'guest')
+  console.log('Guest User');
+  else console.log('Unknown User')
