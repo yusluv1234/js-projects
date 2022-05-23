b89b966cb8f794b9d719527f4156155442b5c2e6
yusluv1234 <yusluv124@gmail.com>
2022-05-24 00:34:38 +0100

    on the while loop

--- js-understanding-practice/index.js
+++ js-understanding-practice/index.js
@@ -1,42 +1,48 @@
-// Hour 
-// If hour is between 6am and 12pm: Good morning!
-// If it is between 12pm and 6pm: Good afternoon!
-// Otherwise: Good evening!
+// // Hour 
+// // If hour is between 6am and 12pm: Good morning!
+// // If it is between 12pm and 6pm: Good afternoon!
+// // Otherwise: Good evening!
 
-let hour = 19;
-if (hour >= 6 && hour <= 12) 
-   console.log("Good Morning!")
- else if (hour >= 12 && hour <= 18) 
-  console.log("Good Afternoon!")
- else
- console.log("Good Evening!")
+// let hour = 19;
+// if (hour >= 6 && hour <= 12) 
+//    console.log("Good Morning!")
+//  else if (hour >= 12 && hour <= 18) 
+//   console.log("Good Afternoon!")
+//  else
+//  console.log("Good Evening!")
 
-let role;
-switch(role) {
-  case 'guest':
-  console.log('Guest User')
-  break;
-  case 'moderator':
-    console.log('Moderator User')
-    break;
-    default:
-      console.log('Unknown User')
-}
+// let role;
+// switch(role) {
+//   case 'guest':
+//   console.log('Guest User')
+//   break;
+//   case 'moderator':
+//     console.log('Moderator User')
+//     break;
+//     default:
+//       console.log('Unknown User')
+// }
 
-// Using If and Else for this switch and case
-if (role === 'moderator')
-  console.log('Moderator User');
-else if (role === 'guest')
-  console.log('Guest User');
-  else console.log('Unknown User')
+// // Using If and Else for this switch and case
+// if (role === 'moderator')
+//   console.log('Moderator User');
+// else if (role === 'guest')
+//   console.log('Guest User');
+//   else console.log('Unknown User')
 
-// LOOPS for, while, Do...while, for...in, for...of
-// for (initialExpression, condition, increment)
+// // LOOPS for, while, Do...while, for...in, for...of
+// // for (initialExpression, condition, increment)
 
-for (let i = 1; i <= 5; i++) {
-  console.log('Hello World', i)
-}
+// for (let i = 1; i <= 5; i++) {
+//   console.log('Hello World', i)
+// }
 
-for (let i = 0; i <= 5; i++) {
-  if (i % 2 !== 0) console.log(i) // if it's !== that's Odd number but If == that's Even Number
+// for (let i = 0; i <= 5; i++) {
+//   if (i % 2 !== 0) console.log(i) // if it's !== that's Odd number but If == that's Even Number
+// }
+
+let i = 0;
+while(i <= 5) {
+  if (i % 2 !== 0) console.log(i);
+  i++
 }
