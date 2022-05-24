e2537a55c3753c1c0b5e8b75d7d75ee3f842b6ca
yusluv1234 <yusluv124@gmail.com>
2022-05-24 08:38:36 +0100

    Es6 for of loop

--- js-understanding-practice/index.js
+++ js-understanding-practice/index.js
@@ -56,13 +56,17 @@
 // } while(i < 5)
 
 // For In Loop
-const person = {
-  name: 'Jolayemi', age: 24,
-};
+// const person = {
+//   name: 'Jolayemi', age: 24,
+// };
 
-for (let key in person)
-console.log(key, person[key])
+// for (let key in person)
+// console.log(key, person[key])
+
+// const colors = ['red', 'green', 'blue']
+// for (let index in colors)
+//   console.log(index, colors[index]);
 
 const colors = ['red', 'green', 'blue']
-for (let index in colors)
-  console.log(index, colors[index]);
+for (let color of colors)
+  console.log(color)
