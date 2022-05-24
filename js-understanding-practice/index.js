d54f11aa7176de44d70d96bdaa03c2f1fd99f2e6
yusluv1234 <yusluv124@gmail.com>
2022-05-24 09:41:05 +0100

    still on FizzBuzz

--- js-understanding-practice/index.js
+++ js-understanding-practice/index.js
@@ -88,15 +88,15 @@
   console.log(output);
 
   function fizzBuzz(input) {
-    if (typeof input !== 'number'){
+    if (typeof input !== 'number')
     return NaN;
-    }else if ((input % 3 === 0) && (input % 5 === 0)){
+    if ((input % 3 === 0) && (input % 5 === 0))
     return "FizzBuzz";
-  }else if (input % 3 === 0){
+    if (input % 3 === 0)
     return "Fizz";
-  }else if (input % 5 === 0){
+    if (input % 5 === 0)
     return "Buzz";
-  }
+
   return input;
 
   }
