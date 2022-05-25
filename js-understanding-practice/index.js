04c261b1461e80eb6f310a8e22217266e1148abb
yusluv1234 <yusluv124@gmail.com>
2022-05-25 12:17:21 +0100

    FizzBuzz

--- js-understanding-practice/index.js
+++ js-understanding-practice/index.js
@@ -84,19 +84,19 @@
   //   return (width > height);
   // }
 
-  const output = fizzBuzz(7);
-  console.log(output);
+  // const output = fizzBuzz(7);
+  // console.log(output);
 
-  function fizzBuzz(input) {
-    if (typeof input !== 'number')
-    return NaN;
-    if ((input % 3 === 0) && (input % 5 === 0))
-    return "FizzBuzz";
-    if (input % 3 === 0)
-    return "Fizz";
-    if (input % 5 === 0)
-    return "Buzz";
+  // function fizzBuzz(input) {
+  //   if (typeof input !== 'number')
+  //   return NaN;
+  //   if ((input % 3 === 0) && (input % 5 === 0))
+  //   return "FizzBuzz";
+  //   if (input % 3 === 0)
+  //   return "Fizz";
+  //   if (input % 5 === 0)
+  //   return "Buzz";
 
-  return input;
+  // return input;
 
-  }
+  // }
