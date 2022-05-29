194c30584e39e317e751ddafcea1c5d68e71498c
yusluv1234 <yusluv124@gmail.com>
2022-05-30 05:23:33 +0800

    check speed

--- js-understanding-practice/index.js
+++ js-understanding-practice/index.js
@@ -115,17 +115,17 @@
 
   // checkSpeed(130);
   
-  function checkSpeed(speed) {
-    const speedLimit = 70;
-    const kmPerPoint = 5;
+  // function checkSpeed(speed) {
+  //   const speedLimit = 70;
+  //   const kmPerPoint = 5;
 
-    if (speed < speedLimit + kmPerPoint)
-    console.log('Okay');
-    else {
-      const points = Math.floor((speed - speedLimit) / kmPerPoint);
-      if(points >= 12)
-      console.log('License suspended');
-      else
-      console.log('Points', points);
-    }
-  }
+  //   if (speed < speedLimit + kmPerPoint)
+  //   console.log('Okay');
+  //   else {
+  //     const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  //     if(points >= 12)
+  //     console.log('License suspended');
+  //     else
+  //     console.log('Points', points);
+  //   }
+  // }
